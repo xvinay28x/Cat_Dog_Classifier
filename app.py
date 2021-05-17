ebaa085e3ff06a9fb94328a3ae04e880f69a887c
Vinay Garg <xvinay28x@gmail.com>
2021-05-18 01:30:17 +0530

    -

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template("index.html",img_ads = "statics/image/send.png" )
+    return render_template("index.html",img_ads = "static/image/send.png" )
 
 
 @app.route('/predict', methods=['POST'])
 def home():
     image = request.files["image"]
     save = image.save("static\image.jpg")
-    load_image = tf.keras.preprocessing.image.load_img("statics\image.jpg",target_size=(200,200))
+    load_image = tf.keras.preprocessing.image.load_img("static\image.jpg",target_size=(200,200))
     image_array = tf.keras.preprocessing.image.img_to_array(load_image)
     reshape_array = image_array.reshape(1,200,200,3)
     image = reshape_array/255
@@ -26,7 +26,7 @@ def home():
     else:
         result = "It is a Cat"
     
-    return render_template("index.html", result = result , img_ads = "statics\image.jpg" )
+    return render_template("index.html", result = result , img_ads = "static\image.jpg" )
 
 if __name__ == "__main__": 
     app.run(debug=True)    
